2ce63e88f7ae55b6ce034b0bc7f18fe6036579cd
elliemeis923 <elliemeis923@gmail.com>
2022-09-16 12:58:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/objectorientedprogrammingpart2/MainActivity.java
@@ -0,0 +1,114 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+package com.example.objectorientedprogrammingpart2;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.objectorientedprogrammingpart2.R;
+
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+
+    int quantity = 2;
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_price);
+        priceTextView.setText(message);
+    }
+
+    public void submitOrder(View view) {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean whippedCream = whippedCreamCheckBox.isChecked();
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean chocolate = chocolateCheckBox.isChecked();
+        createOrderSummary(quantity);
+    }
+
+    public void increment(View view){
+        quantity++;
+        displayQuantity(quantity);
+    }
+    public void decrement(View view){
+        quantity--;
+        displayQuantity(quantity);
+    }
+    /**
+     * Calculates the price of the order based on the current quantity.
+     *
+     * @return the price
+     */
+    private int calculatePrice() {
+        int price = 5;
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        if(whippedCreamCheckBox.isChecked()){price ++;}
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        if(chocolateCheckBox.isChecked()){price += 2;}
+        return quantity * price;
+    }
+    private void createOrderSummary(int quantity) {
+        TextView creamView = (TextView) findViewById(R.id.order_summary_cream);
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        if(whippedCreamCheckBox.isChecked())
+            {creamView.setText("Add whipped cream? true");}
+        else{creamView.setText("Add whipped cream? false");}
+        TextView chocView = (TextView) findViewById(R.id.order_summary_chocolate);
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        if(chocolateCheckBox.isChecked())
+            {chocView.setText("Add chocolate? true");}
+        else{chocView.setText("Add chocolate? false");}
+
+        TextView quantityView = (TextView) findViewById(R.id.order_summary_quantity);
+        quantityView.setText("Quantity: " + quantity);
+        TextView orderSummaryPrice = (TextView) findViewById(R.id.order_summary_price);
+        orderSummaryPrice.setText("Price: $" + calculatePrice());
+
+
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int quantity) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + quantity);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView orderSummaryPrice = (TextView) findViewById(R.id.order_summary_price);
+        orderSummaryPrice.setText("Price: $" + NumberFormat.getCurrencyInstance().format(number));
+    }
+}
